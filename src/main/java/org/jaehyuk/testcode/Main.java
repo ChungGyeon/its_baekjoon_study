12b90ce81a0b14edf4fcc87b3f969d1a987ec9a2
임재혁 <imsdm@naver.com>
2025-06-01 23:11:34 +0900

    week5: 색종이

--- src/main/java/org/jaehyuk/testcode/Main.java
+++ src/main/java/org/jaehyuk/testcode/Main.java
@@ -2,27 +2,27 @@ package org.jaehyuk.testcode;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] arg){
         Scanner sc =new Scanner(System.in);
-        ArrayList<String> arr=new ArrayList<>();
-        while(sc.hasNext()){
-            String input= sc.nextLine();
-            if(input.equals("-")) break;
-            arr.add(input);
-        }
-
-        int max=0;
-        for(String s: arr){
-            max=Math.max(max,s.length());
-        }
-
-        for(int i=0; i<max; i++){
-            for(int j=0; j<arr.size(); j++){
-                if(i<arr.get(j).length()){
-                    System.out.print(arr.get(j).charAt(i));
+        int cnt=sc.nextInt();
+        boolean[][] paper=new boolean[100][100];
+        for(int i=0; i<cnt; i++){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            for(int height=x; height<x+10; height++){
+                for(int width=y; width<y+10; width++){
+                    paper[height][width]=true;
                 }
             }
         }
 
+        cnt=0;
+        for(int i=0; i<100; i++){
+            for(int j=0; j<100; j++){
+                if(paper[i][j]) cnt++;
+            }
+        }
+        System.out.println(cnt);
     }
 }
