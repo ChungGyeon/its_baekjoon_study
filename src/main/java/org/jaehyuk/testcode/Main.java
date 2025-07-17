086025dba45729eaca7590f405dad928b456ac1a
임재혁 <imsdm@naver.com>
2025-07-17 11:15:47 +0900

    11659 구간 합 구하기
    합배열을 이용하는 문제
    구간 합 공식 S[i]-S[i-1]이므로 이 문제에선 각 i,j를 채우면 되겠다고 판단했다.
    다만 범위가 1~3이라고 했을 때는 단순히 S[j]만 출력하면 되는 것을 이걸 안하고 있어서 계속 오래걸렸다.
    정신 나가겠네

--- src/main/java/org/jaehyuk/testcode/Main.java
+++ src/main/java/org/jaehyuk/testcode/Main.java
@@ -1,25 +1,46 @@
 package org.jaehyuk.testcode;
 import java.io.*;
 import java.util.*;
+/*
+수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다. 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
 
+총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+
+범위
+1 ≤ N ≤ 100,000
+1 ≤ M ≤ 100,000
+1 ≤ i ≤ j ≤ N
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        int envelp=Integer.parseInt(sc.readLine());
-        int cnt=0;
-        while (true){
-            if(envelp%5==0){
-                cnt+=envelp / 5;
-                break;
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int A[] = new int[N];
+        int S[] = new int[N];
+
+        for (int i = 0; i< N; i++){
+            A[i] = sc.nextInt();
+        }
+        S[0]=A[0];
+        for(int i = 1; i < N; i++){
+            S[i]=S[i-1]+A[i];
+        }
+        int i=0;
+        int j=0;
+        for(int k=0; k<M; k++){
+            i=sc.nextInt();
+            j=sc.nextInt();
+            i-=1;
+            j-=1;
+            if(i==0){
+                System.out.println(S[j]);
             }
-            envelp-=3;
-            cnt++;
-            if(envelp<0){
-                cnt=-1;
-                break;
+            else {
+                System.out.println(S[j] - S[i - 1]);
             }
         }
-        System.out.println(cnt);
         sc.close();
     }
 }
