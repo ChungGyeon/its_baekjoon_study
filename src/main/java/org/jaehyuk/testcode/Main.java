2a175b5f66c83002d299ba5ce1b44486e7223c08
임재혁 <imsdm@naver.com>
2025-08-11 10:53:12 +0900

    기록을 안하고 있었네, 1253 좋은 수-1번째 트라이
    결과는 당연히 실패,
    테스트 코드 10으로는 성공함
    실제 답안지에선 3%에서 실패
    뭐가 문제지

--- src/main/java/org/jaehyuk/testcode/Main.java
+++ src/main/java/org/jaehyuk/testcode/Main.java
@@ -1,46 +1,38 @@
 package org.jaehyuk.testcode;
 import java.io.*;
 import java.util.*;
-/*
-수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
-첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다. 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
 
-총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
-
-범위
-1 ≤ N ≤ 100,000
-1 ≤ M ≤ 100,000
-1 ≤ i ≤ j ≤ N
- */
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int A[] = new int[N];
-        int S[] = new int[N];
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        double[] arr = new double[n];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Double.parseDouble(st.nextToken());
+        }
+        int start = 0, count = 0;
+        int end = n-1, tmp = n-1;
+        Arrays.sort(arr);
+        double target = arr[end];
+        try {
+            while (start < end) {
+                if (target == arr[start] + arr[end]) {
+                    end--;
+                    tmp--;
+                    target = arr[tmp];
+                    count++;
+                } else if (target < arr[start] + arr[end]) {
+                    end--;
+                } else if (target > arr[start] + arr[end]) {
+                    start++;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println(count);
 
-        for (int i = 0; i< N; i++){
-            A[i] = sc.nextInt();
-        }
-        S[0]=A[0];
-        for(int i = 1; i < N; i++){
-            S[i]=S[i-1]+A[i];
-        }
-        int i=0;
-        int j=0;
-        for(int k=0; k<M; k++){
-            i=sc.nextInt();
-            j=sc.nextInt();
-            i-=1;
-            j-=1;
-            if(i==0){
-                System.out.println(S[j]);
-            }
-            else {
-                System.out.println(S[j] - S[i - 1]);
-            }
-        }
-        sc.close();
     }
 }
+//1 2 3 4 5 7
