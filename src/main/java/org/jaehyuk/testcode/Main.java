9c636d29946669633b275b270045bb59bb31b1e3
임재혁 <imsdm@naver.com>
2025-08-12 09:50:10 +0900

    12891번 슬라이딩 윈도우 - 1번째 실패
    이번엔 예제 코드뿐만 아니라, 최소, 최대한번에 생각해서 만들었어, 어제 조금 만들고 오늘 또 시작한거지만 ㅋㅋ...
    어쨋든 코드는 실패, 내가 상정한 예제입력들은 다 통과했지만 시간이 오버 됨
    이중 for문이 문제로 보여.
    
    그리고 문제는 지금 생각한 로직은 이중for문말고는 생각이 안난단 말이지...
    조금 더 생각해보고 10시20분?쯤에 답보고 비교할예정

--- src/main/java/org/jaehyuk/testcode/Main.java
+++ src/main/java/org/jaehyuk/testcode/Main.java
@@ -5,63 +5,58 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        double[] arr = new double[n];
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        String DNA = bf.readLine(); //역시 문자열은 이게 맞아
+
+        int[] minctn = new int [4]; //최소개수 저장배열
+        st = new StringTokenizer(bf.readLine());
+        for(int i=0; i<4; i++){
+            minctn[i] = Integer.parseInt(st.nextToken());
         }
-        int start = 0, count = 0, tmp = 0;
-        int end = n - 1;
-        Arrays.sort(arr);
-        double target = arr[tmp];
-        for(int i=0; i<n; i++){
-            start=0;
-            end=n-1;
-            target = arr[i];
-         while (start < end) {
-            if (target == arr[start] + arr[end]) {
-                if(start != i && end != i ){
-                    count++;
-                    break;
-                }
-                else if (start == i){
-                    start++;
-                }
-                else if (end == i){
-                    end--;
-                }
-            } else if (target < arr[start] + arr[end]) {
-                end--;
-            } else{
-                start++;
+
+        int[] dect = new int [4];
+        for(int i=0; i<DNA.length(); i++){
+            if(DNA.charAt(i) == 'A') dect[0]++;
+            if(DNA.charAt(i) == 'C') dect[1]++;
+            if(DNA.charAt(i) == 'G') dect[2]++;
+            if(DNA.charAt(i) == 'T') dect[3]++;
+        }
+        //애초에 개수 안맞는 예외처리
+        if(dect[0]<minctn[0] || dect[1]<minctn[1] || dect[2]<minctn[2] || dect[3]<minctn[3]){
+            System.out.println(0);
+            return;
+        }
+
+        int start = 0, count = 0, end = 0;
+        if(p==1) {
+            end = 1;
+        }
+        else{
+            end = p;
+        }
+        for(int i=0; i<(s-p)+1; i++){
+            dect[0]=0;
+            dect[1]=0;
+            dect[2]=0;
+            dect[3]=0;
+            for(int j = start; j<end; j++){
+                if(DNA.charAt(j) == 'A') dect[0]++;
+                if(DNA.charAt(j) == 'C') dect[1]++;
+                if(DNA.charAt(j) == 'G') dect[2]++;
+                if(DNA.charAt(j) == 'T') dect[3]++;
             }
+            if(dect[0]<minctn[0] || dect[1]<minctn[1] || dect[2]<minctn[2] || dect[3]<minctn[3]){
+                start++;
+                end++;
+            }
+            else{
+                count++;
+                start++;
+                end++;
             }
         }
         System.out.println(count);
-
     }
 }
-
-/*
-* 주요 문제점
-* 1.타겟 선택 방식이 잘못 됨
-* 모든 수를 타겟으로 검색해야한다.
-*
-* 2.중복값 처리 로직 오류
-* arr[start] ==arr[start+1]은 start+1이게 배열의 범위를 벗어날 수 있음?
-* 이건 while문 조건으로 일어나지 않을 일이고
-*
-* 3.투 포인터 알고리즘 구현 오류
-* 타겟이 바뀔때마다 포인터를 초기화 해야한다.
-* 현재는 이전 상태를 유지하고 있다
-*
-* 4. 자신 자신을 사용하는 경우 처리 부족
-* 타겟 수 자체는 합을 만들때 사용하면 안된다.
-*
-* 3번이 가장 이 문제에 적절할거 같은데?
-* 1번은 내가 끝에서부터 시작하는걸 지적한거야,근데 이것도 문제 없는거 아닌가?
-* 인덱스 0 부터 시작하게 하는건 문제가 있지, 지금 코드로는
-* 만약 0부터 시작해서 아무것도 안맞는다면 target을 늘리고 하는 식으로 가야겠네
-*
-* 위 조건대로 해봐도 안되서 답지 보고 만듬*/
