fc5d726381edd20b583a7faccfa7b89b8d187305
임재혁 <imsdm@naver.com>
2025-08-12 20:58:19 +0900

    12891번 슬라이딩 윈도우 7번째 시도 - 성공
    결국 AI보고 답 맞춤, 문제는 이 로직을 생각해냈다가 현타와서 모든 로직이 내 머리속에 사라졌다는거지
    
    이전에 생각한 문제, 코드를 짧게 해야한다는 강박으로 인해 이 코드를 만들 수 없었던 걸 수 도 있겠어

--- src/main/java/org/jaehyuk/testcode/Main.java
+++ src/main/java/org/jaehyuk/testcode/Main.java
@@ -8,6 +8,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int s = Integer.parseInt(st.nextToken());
         int p = Integer.parseInt(st.nextToken());
+
         String DNA = bf.readLine(); //역시 문자열은 이게 맞아
 
         int[] minctn = new int [4]; //최소개수 저장배열
@@ -16,44 +17,31 @@ public class Main {
             minctn[i] = Integer.parseInt(st.nextToken());
         }
 
-
-        int[] dect = new int [4];
+        int[] dect = new int[4];
         int count = 0;
 
-        for(int i=0; i<p; i++){ //첫 라인 미리 계산
-            if(DNA.charAt(i) == 'A') dect[0]++;
-            if(DNA.charAt(i) == 'C') dect[1]++;
-            if(DNA.charAt(i) == 'G') dect[2]++;
-            if(DNA.charAt(i) == 'T') dect[3]++;
+
+        for(int i=0; i<p; i++){
+            if(DNA.charAt(i)=='A') dect[0]++;
+            if(DNA.charAt(i)=='C') dect[1]++;
+            if(DNA.charAt(i)=='G') dect[2]++;
+            if(DNA.charAt(i)=='T') dect[3]++;
         }
-        if (dect[0] >= minctn[0] && dect[1] >= minctn[1] && dect[2] >= minctn[2] && dect[3] >= minctn[3]) {
-            count++;
-        }
+        if(dect[0]>=minctn[0] && dect[1]>=minctn[1] && dect[2]>=minctn[2] && dect[3]>=minctn[3]) count++;
 
-            for (int i = p; i < s; i++) {
-                if (DNA.charAt(i) == 'A') {
-                    dect[0]--;
-                } else if (DNA.charAt(i) == 'C') {
-                    dect[1]--;
-                } else if (DNA.charAt(i) == 'G') {
-                    dect[2]--;
-                } else if (DNA.charAt(i - p) == 'T') {
-                    dect[3]--;
-                }
+        for(int i=p; i<s; i++){
+            char tmp = DNA.charAt(i-p);
+            if(tmp=='A') dect[0]--;
+            if(tmp=='C') dect[1]--;
+            if(tmp=='G') dect[2]--;
+            if(tmp=='T') dect[3]--;
 
-                if (DNA.charAt(i) == 'A') {
-                    dect[0]++;
-                } else if (DNA.charAt(i) == 'C') {
-                    dect[1]++;
-                } else if (DNA.charAt(i) == 'G') {
-                    dect[2]++;
-                } else if (DNA.charAt(i) == 'T') {
-                    dect[3]++;
-                }
-
-                if (dect[0] >= minctn[0] && dect[1] >= minctn[1] && dect[2] >= minctn[2] && dect[3] >= minctn[3]) {
-                    count++;
-                }
+            tmp = DNA.charAt(i);
+            if(tmp=='A') dect[0]++;
+            if(tmp=='C') dect[1]++;
+            if(tmp=='G') dect[2]++;
+            if(tmp=='T') dect[3]++;
+            if(dect[0]>=minctn[0] && dect[1]>=minctn[1] && dect[2]>=minctn[2] && dect[3]>=minctn[3]) count++;
         }
         System.out.println(count);
     }
